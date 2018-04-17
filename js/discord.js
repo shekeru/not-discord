003f4fce70379c75f04a9201d2929395e479c436
Unknown <wish@sek.ai>
2018-04-17 19:39:03 +1000

    things are just better this way

--- js/discord.js
+++ js/discord.js
@@ -1,14 +1,28 @@
 document.getElementById("status").innerHTML = "Waiting...";
 function connect() {
-  document.getElementById("connection").classList += " vanish";
-  document.getElementById("status").innerHTML = "Connecting...";
   function heartbeat() {
     console.log("Lub...");
     socket.send(JSON.stringify({"op":1,"d":{}}));
   }
+  async function decrement() {
+    for (var i = 0; i < Object.keys(typing_users).length; i++) {
+      typing_users[Object.keys(typing_users)[i]] -= 1;
+      if (typing_users[Object.keys(typing_users)[i]] == 0) {
+        delete typing_users[Object.keys(typing_users)[i]];
+        typing -= 1;
+      }
+    }
+  }
   const guilds = {};
+  var typing = 0;
+  var typing_users = {};
   const client_token = document.getElementById("token").value;
   const socket = new WebSocket("wss://gateway.discord.gg/?v=6&encoding=json");
+  document.getElementById("connection").classList += " vanish";
+  document.getElementById("status").innerHTML = "Connecting...";
+  document.getElementById("server-list").classList += " appear";
+  document.getElementById("user-float").classList += " move";
+  document.getElementById("typing-status").innerHTML = typing + " users are typing."
   socket.onmessage = function(event) {
     let recv = JSON.parse(event.data);
     switch (recv.op) {
@@ -44,15 +58,32 @@ function connect() {
           string.append(at);
           string.append(guild);
           string.append(message);
+          if (document.getElementById("msg-list").childNodes.length == 100) {
+            document.getElementById("msg-list").removeChild(document.getElementById("msg-list").childNodes[0]);
+          }
           document.getElementById("msg-list").appendChild(string);
           window.scrollTo(0, document.body.scrollHeight);
         } else if (recv.t === "READY") {
-          console.log(recv);
+          setInterval(decrement, 1000);
           document.getElementById("status").innerHTML = recv.d.user.username + "#" + recv.d.user.discriminator;
           for (var i = 0; i < recv.d.guilds.length; i++) {
             guilds[recv.d.guilds[i].id] = recv.d.guilds[i].name;
+            let string = document.createElement("DIV");
+            let guild = document.createElement("SPAN");
+            let members = document.createElement("SPAN");
+            string.id = recv.d.guilds[i].id;
+            guild.id = "guild";
+            members.id = "members";
+            guild.append(recv.d.guilds[i].name);
+            members.append(": " + recv.d.guilds[i].member_count);
+            string.append(guild);
+            string.append(members);
+            document.getElementById("server-list").appendChild(string);
           }
-          console.log(guilds);
+        } else if (recv.t === "TYPING_START") {
+          typing_users[recv.d.user_id] = 5;
+          typing += 1;
+          document.getElementById("typing-status").innerHTML = typing + " users are typing."
         }
         break;
       case 1:
